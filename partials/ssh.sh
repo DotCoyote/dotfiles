71c54f2fe970ff89274600d047235c22a3509fe6
Lars Eicher <lars.eichler@appsfactory.de>
2020-12-22 19:21:52 +0100

    Prompt for emailaddress

--- partials/ssh.sh
+++ partials/ssh.sh
@@ -6,16 +6,12 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-if ls ~/.ssh/id_rsa 2>/dev/null
-then
-    echo "SSH Key already exists"
+read -p "SSH E-Mail-Address: " mail
 
-else
-    # generate ssh Key
-    echo "Generate ssh Key"
-    ssh-keygen -t rsa -b 4096 -C "larseichler.le@gmail.com"
+# generate ssh Key
+echo "Generate SSH Key"
+ssh-keygen -t rsa -b 4096 -C $mail
 
-    # add key to ssh agent
-    echo "Add generated Key to ssh-agent"
-    ssh-add ~/.ssh/id_rsa
-fi
+# add key to ssh agent
+echo "Add generated Key to ssh-agent"
+ssh-add ~/.ssh/id_rsa
